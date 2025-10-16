994384372bce7ec350a41e2247b334121e7c913b
Kieran Hunt <kph@hotmail.ca>
2025-10-16 11:32:36 -0400

    Update simt_hgemv.cu

--- src/simt_hgemv.cu
+++ src/simt_hgemv.cu
@@ -20,7 +20,7 @@ inline void checkCudaStatus(cudaError_t status)
     }
 }
 
-__device__ __forceinline__ half warpReduceSum(half sum, uint32_t threadNum, unsigned long long mask = 0xffffffff)
+__device__ __forceinline__ half warpReduceSum(half sum, uint32_t threadNum, unsigned long long mask = 0xFFFFFFFFFFFFFFFF)
 {
     if (threadNum >= 32)
         sum += __shfl_down_sync(mask, sum, 16); // 0-16, 1-17, 2-18, etc.
